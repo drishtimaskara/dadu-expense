7aa9b197aeae3cb2d8e85e21905c533b23e7bfe7
Drishti Maskara <drishtimaskara@gmail.com>
2026-06-08 04:45:18 +0530

    back to original

--- frontend/src/services/api.jsx
+++ frontend/src/services/api.jsx
@@ -1,7 +1,9 @@
 import axios from "axios";
 
-const API = axios.create({
-  baseURL: "https://dadu-expense.onrender.com/"
+const API =
+axios.create({
+ baseURL:
+ "http://localhost:5000/api"
 });
 
 export default API;
